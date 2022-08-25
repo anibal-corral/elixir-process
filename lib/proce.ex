d8020e300c2561ccaa45e79e31a58f144b9d3fbf
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-25 09:56:19 -0400

    Process link

--- lib/proce.ex
+++ lib/proce.ex
@@ -6,15 +6,27 @@ defmodule Proce do
 # This is with messages
 def hello(count) do
   receive do
-    [:quit] ->
+    #exit when the reason is a "normal" reason.
+    {:crash, reason} ->
+      exit(reason)
+    #exit when the resaon is an "anormal" reason
+    {:crash, reason} ->
+      exit(reason)
+    {:quit} ->
       IO.puts "I'm outta here"
-    [:reset]->
+    {:reset}->
       hello(0)
-    [:add, n]->
+    {:add, n}->
       hello(count+n)
     msg ->
       IO.puts("#{count}: Hello #{inspect msg}")
       hello(count)
   end
+end
+
+def zombie() do
+  inner_process = fn -> Process.sleep(10000) end
+  outer_process = fn -> spawn_link(inner_process);exit(:bad) end
+  Enum.each(1..100, fn _ -> spawn(outer_process)end)
 end
 end
